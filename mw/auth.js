ac9ed3e07e61a7672e7a2685ebdbd1267f28a98e
vleypi <vleypi@gmail.com>
2022-11-15 18:32:26 +0500

    auth mw

---
+++ mw/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req,res,next) =>{
+    if(req.method === "OPTIONS"){
+        next()
+    }
+
+    try{
+        const token = req.headers.authorization.split(' ')[1]
+        
+        if(!token){
+            return res.status(403).json({mes: 'Un auth 1'})
+        }
+
+        const decoded = jwt.verify(token,process.env.SECRETKEY)
+        req.user = decoded
+        next()
+    }
+    catch(err){
+        console.log(err)
+        return res.status(403).json({mes: 'Un auth 2'})
+    }
+}
