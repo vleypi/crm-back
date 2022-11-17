c09d10e1ebd36588e5221f7459bf889292fdd1a0
vleypi <vleypi@gmail.com>
2022-11-17 23:18:57 +0500

    lesson

--- controllers/lessonsController.js
+++ controllers/lessonsController.js
@@ -9,10 +9,22 @@ class LessonsController {
 
     async getLessons(req,res) {
         try{
-            const lessons = await parse(await request('SELECT * FROM '))
+            const lessons = await parse(await request('SELECT * FROM lessons'))
         }
         catch(err){
             console.log(err)
+        }
+    }
+
+    async addLessons(req,res){
+        try{
+            const {lesson_type,lesson_title,lesson_desc,lesson_color} = req.body
+            parse(await request("INSERT INTO `tokens` (`lesson_id`,`lesson_type`,`lesson_title`,`lesson_desc`,`lesson_color`) VALUES('" + shortid.generate() + "','" + emailCheck[0].user_id + "','" + refreshToken + "')")) 
+
+            await parse(await request("INSERT INTO `lessons` ()"))
+        }
+        catch(err){
+
         }
     }
 }
