1f63d9c352f63e0485d2708d1f30e3127419b51a
Vleypi <vleypi@gmail.com>
2022-11-22 16:12:48 +0500

    lessons updates

--- controllers/lessonsController.js
+++ controllers/lessonsController.js
@@ -9,18 +9,32 @@ class LessonsController {
 
     async setLessons(req,res){
         try{
-            const {lesson_name,lesson_color,lesson_type,lesson_teachers,lesson_students} = req.body
+            const validate = validationResult(req)
 
+            if(!validate.isEmpty()){
+                return res.status(400).json({mes: validate.errors[0].msg})
+            }
+            
+            const {lesson_name,lesson_color,lesson_type,lesson_teachers,lesson_students} = req.body
 
             const lesson_id = shortid.generate()
 
             await parse(await request("INSERT INTO `lessons` (`lesson_id`,`lesson_name`,`lesson_color`,`lesson_status`,`lesson_type`) VALUES('" + lesson_id + "','" + lesson_name + "','" + lesson_color + "','" + 1 + "','" + lesson_type +"')")) 
 
-            lesson_students.map(async (student)=>{
-                await parse(await request("INSERT INTO `lessons_user` (`lesson_id`, `user_id`,`role`) VALUES('"+ lesson_id +"','"+ student.user_id +"','"+ student.role +"')"))
-            })
+            if(lesson_students.length){
+                console.log(lesson_students,'students')
+                lesson_students.map(async (student)=>{
+                    await parse(await request("INSERT INTO `lessons_user` (`lesson_id`, `user_id`,`role`) VALUES('"+ lesson_id +"','"+ student.user_id +"','"+ student.role +"')"))
+                })
+            }
             
-            
+            if(lesson_teachers.length){
+                lesson_teachers.map(async (teacher)=>{
+                    await parse(await request("INSERT INTO `lessons_user` (`lesson_id`, `user_id`,`role`) VALUES('"+ lesson_id +"','"+ teacher.user_id +"','"+ teacher.role +"')"))
+                })
+            }
+
+            return res.status(200).json({})
         }   
         catch(err){
             console.log(err)
@@ -35,7 +49,7 @@ class LessonsController {
             res.status(200).json({
                 students:students.map((student)=>{
                     return {
-                        value: student.name,
+                        value: student.user_id,
                         label: student.name,
                         name: student.name,
                         user_id: student.user_id,
@@ -44,7 +58,7 @@ class LessonsController {
                 }),
                 teachers:teachers.map((teacher)=>{
                     return {
-                        value: teacher.name,
+                        value: teacher.user_id,
                         label: teacher.name,
                         name: teacher.name,
                         user_id: teacher.user_id,
--- routes/lessonsRouter.js
+++ routes/lessonsRouter.js
@@ -9,7 +9,13 @@ const lessonsController = require('../controllers/lessonsController')
 const auth = require('../mw/auth')
 const role = require('../mw/role')
 
-router.post('/setLessons',auth,lessonsController.setLessons)
+router.post('/setLessons',auth,[
+    check('lesson_type','Что-то пошло не так').notEmpty(),
+    check('lesson_name','Минимальная длина названия 1 символ').isLength({min: 1}),
+    check('lesson_color','Что-то пошло не так').notEmpty()
+],lessonsController.setLessons)
+
+
 router.post('/getStudentsTeachers',auth,lessonsController.getStudentsTeachers)
  
 module.exports = router
